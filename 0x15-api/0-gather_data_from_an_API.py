ebee4534c1e26a2217da7d7af7e632c7e6e74487
JoyEmma12 <ejoy0600@gmail.com>
2023-05-27 21:43:03 +0100

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script that uses REST API 
+for an employee and returns inoformation
+about his/her TODO list
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    """
+    Employee TODO list output
+    """
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userid": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
