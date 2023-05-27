13c2c9b71ac1c8cba961c1e1a90183d7ccf9148b
JoyEmma12 <ejoy0600@gmail.com>
2023-05-27 21:55:00 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,17 +5,36 @@ for an employee and returns inoformation
 about his/her TODO list
 """
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     """
     Employee TODO list output
     """
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userid": sys.argv[1]}).json()
+    try:
+        emply_id = int(argv[1])
+    except ValueError:
+        exit()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+
+    url = 'https://jsonplaceholder.typicode.com'
+    user = '{api}/users/{id}'.format(api=api_url, id=emp_id)
+    todo = '{user_uri}/todos'.format(user_uri=user_uri)
+
+    res = requests.get(url).json()
+
+    name = res.get('name')
+
+    res = requests.get(todo).json()
+
+    total = len(res)
+
+    non_completed = sum([elem['completed'] is False for elem in res])
+
+    completed = total - non_completed
+    str = "Employee {emp_name} is done with tasks({completed}/{total}):"
+    print(str.format(emp_name=name, completed=completed, total=total))
+
+    for elem in res:
+        if elem.get('completed') is True:
+            print('\t', elem.get('title'))
